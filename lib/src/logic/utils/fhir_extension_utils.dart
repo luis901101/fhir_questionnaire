8e74e4fca4cabe83c204575a66b6245e39e0c0f4
sh1l0n <jesusmanresaparres@protonmail.com>
2025-04-09 06:27:34 +0200

    Add documentation

--- lib/src/logic/utils/fhir_extension_utils.dart
+++ lib/src/logic/utils/fhir_extension_utils.dart
@@ -3,6 +3,41 @@ import 'package:fhir/r4.dart';
 import 'package:intl/intl.dart';
 
 extension FhirExtensionUtils on Iterable<FhirExtension> {
+  /// Attempts to localize the content of FHIR extensions based on the provided locale.
+  ///
+  /// This method searches for a translation within the FHIR extensions where:
+  /// - The extension's URL is `http://hl7.org/fhir/StructureDefinition/translation`.
+  /// - It contains a nested extension with a URL of `lang` and a `valueCode` matching
+  ///   the provided `locale` or the default locale (`Intl.defaultLocale`).
+  ///
+  /// If a matching translation is found, it returns the `valueString` or `valueMarkdown`
+  /// of the nested extension with a URL of `content`.
+  ///
+  /// - [locale]: The locale to search for. Defaults to `Intl.defaultLocale` if not provided.
+  ///
+  /// Returns the localized content as a `String`, or `null` if no matching translation is found.
+  ///
+  /// Example:
+  /// ```dart
+  /// final extensions = <FhirExtension>[
+  ///   FhirExtension(
+  ///     url: FhirUri('http://hl7.org/fhir/StructureDefinition/translation'),
+  ///     extension_: [
+  ///       FhirExtension(
+  ///         url: FhirUri('lang'),
+  ///         valueCode: Code('en'),
+  ///       ),
+  ///       FhirExtension(
+  ///         url: FhirUri('content'),
+  ///         valueString: 'Hello, World!',
+  ///       ),
+  ///     ],
+  ///   ),
+  /// ];
+  ///
+  /// final localizedContent = extensions.localize('en');
+  /// print(localizedContent); // Output: Hello, World!
+  /// ```
   String? localize([final String? locale]) {
     final translation = firstWhereOrNull(
       (ext) =>
