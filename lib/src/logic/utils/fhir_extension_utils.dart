2adcc6a86287c2e3cf70b9e5793d6a9985515dca
__sh0l1n@ <jesusmanresaparres@protonmail.com>
2025-10-30 12:19:35 +0100

    Fix comment formatting for locale parameter

--- lib/src/logic/utils/fhir_extension_utils.dart
+++ lib/src/logic/utils/fhir_extension_utils.dart
@@ -16,7 +16,7 @@ extension FhirExtensionUtils on Iterable<FhirExtension> {
   /// If a matching translation is found, it returns the `valueString` or `valueMarkdown`
   /// of the nested extension with a URL of `content`.
   ///
-  /// - [locale]: The locale to search for. Defaults to Intl.defaultLocale or `QuestionnaireLocalization.instance.localization.locale` if not provided.
+  /// - [locale]: The locale to search for. Defaults to `QuestionnaireLocalization.instance.localization.locale` if not provided.
   ///
   /// Returns the localized content as a `String`, or `null` if no matching translation is found.
   ///
